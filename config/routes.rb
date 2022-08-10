e232683ceff4789734a98e8e94ed9f93a2efd87c
keisacct <107605705+keisacct@users.noreply.github.com>
2022-08-10 14:08:48 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index" # ルートパスへのアクセスがあれば、messages_controllerのindexアクションが呼び出される
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
