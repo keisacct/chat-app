c212e39e2ef2db440f2ff72c38f7b0aa1ad11061
keisacct <107605705+keisacct@users.noreply.github.com>
2022-08-10 11:04:03 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
   root to: "messages#index" # ルートパスへのアクセスがあれば、messages_controllerのindexアクションが呼び出される
+  resources :users, only: [:edit, :update]
 end
